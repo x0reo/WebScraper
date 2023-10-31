24112fe653f4ca54acbe091de1f355cae5444306
x0reo <illumeddy@gmail.com>
2023-10-31 21:15:16 +0800

    Added comments

--- webscraper.py
+++ webscraper.py
@@ -2,13 +2,17 @@ from bs4 import BeautifulSoup
 import requests
 
 def scrapeWeb():
-    # pageToScrape = requests.get("https://realpython.github.io/fake-jobs/")
+    # paste url here
     pageToScrape = requests.get("https://www.w3schools.com/html/")
+
     soup = BeautifulSoup(pageToScrape.text, "html.parser")
+
+    # html block elements to read
     paragraphs = soup.findAll('p')
     for paragraph in paragraphs:
         print(paragraph.text)
 
+    # html and css of the website
     print(pageToScrape.text)
 
 scrapeWeb()
