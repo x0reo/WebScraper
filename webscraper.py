559599f330d9c61ce421a8bdb47631835aea10b6
x0reo <illumeddy@gmail.com>
2023-11-02 19:57:42 +0800

    Polished scraping to exclude links and nav bars

--- webscraper.py
+++ webscraper.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import requests, re
 
 # paste url here
-url = "https://www.javatpoint.com/theory-of-automata"
+url = "https://sourcemaking.com/design_patterns"
 pageToScrape = requests.get(url)
 
 # Some urls to test:
@@ -32,11 +32,16 @@ def scrapeAll():
     # Parse the HTML content using BeautifulSoup
     soup = BeautifulSoup(pageToScrape.text, "html.parser")
 
+    def contains_link(element):
+        return element.find('a') is not None
+
     # Elements to exclude
     def is_valid_element(element):
         if element.name == 'a':
             return False
         if element.find_parent('a'):
+            return False
+        if element.find_parent('nav'):
             return False
         if element.find_parent('div', id=lambda value: value and ('menu' in value or 'footer' in value)):
             return False
@@ -44,10 +49,11 @@ def scrapeAll():
 
     # Find all headers (h1 to h6), paragraphs, and their parent elements while excluding links and their child elements
     valid_elements = filter(is_valid_element, soup.recursiveChildGenerator())
+    valid = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'pre', 'li', 'span']
 
     # Iterate through the valid elements and print their text
     for element in valid_elements:
-        if element.name and element.name in ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'pre', 'li']:
+        if element.name and element.name in valid and not contains_link(element):
             print(element.text.strip())
 
 def scrapeWebHtml():
