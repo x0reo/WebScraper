7e764d5f343838eeb2df21bea905dff5521a2de7
x0reo <illumeddy@gmail.com>
2023-11-03 00:02:34 +0800

    Added html to markdown

--- webscraper.py
+++ webscraper.py
@@ -1,8 +1,8 @@
 from bs4 import BeautifulSoup
-import requests, re
+import requests, html2text
 
-# paste url here
-url = "https://sourcemaking.com/design_patterns"
+# Paste url here
+url = "https://www.javatpoint.com/theory-of-automata"
 pageToScrape = requests.get(url)
 
 # Some urls to test:
@@ -10,25 +10,9 @@ pageToScrape = requests.get(url)
     # https://www.w3schools.com/html/
     # https://www.javatpoint.com/theory-of-automata
 
-def scrapeWebText():
 
-    soup = BeautifulSoup(pageToScrape.text, "html.parser")
 
-    # html block elements to read
-    paragraphs = [p.get_text() for p in soup.find_all('p')]
-    headers = [h.get_text() for h in soup.find_all(re.compile('^h[1-6]$'))]
-
-    # Initialize an empty string to store the result
-    result_string = ""
-
-    # Iterate through headers and paragraphs and concatenate them
-    for header, paragraph in zip(headers, paragraphs):
-        result_string += f"{header}\n{paragraph}\n\n"
-
-    # Print the result
-    print(result_string)
-
-def scrapeAll():
+def scrapeToText():
     # Parse the HTML content using BeautifulSoup
     soup = BeautifulSoup(pageToScrape.text, "html.parser")
 
@@ -44,7 +28,7 @@ def scrapeAll():
         if element.find_parent('nav'):
             return False
         if element.find_parent('div', id=lambda value: value and ('menu' in value or 'footer' in value)):
-            return False
+            return False # Divs with id that includes 'menu' and 'footer'
         return True
 
     # Find all headers (h1 to h6), paragraphs, and their parent elements while excluding links and their child elements
@@ -56,11 +40,32 @@ def scrapeAll():
         if element.name and element.name in valid and not contains_link(element):
             print(element.text.strip())
 
-def scrapeWebHtml():
+def scrapeToMarkdown():
 
-    # html and css of the website
+    html_content = pageToScrape.text
+
+    # Parse the HTML content using BeautifulSoup
+    soup = BeautifulSoup(html_content, 'html.parser')
+
+    # Exclude specific elements (e.g., 'nav', 'a' for links)
+    for tag in soup.find_all(['nav', 'a']):
+        tag.extract()
+
+    for div in soup.find_all('div', id=lambda value: value and ('menu' in value or 'footer' in value)):
+        div.extract()
+
+    # Convert the modified HTML to Markdown using html2text
+    markdown_content = html2text.html2text(str(soup))
+
+    # Print the Markdown content
+    print(markdown_content)
+
+def scrapeToHtml():
+    # Html and css of the website
     print(pageToScrape.text)
 
-scrapeAll()
-# scrapeWebText()
-# scrapeWebHtml()
+
+
+# scrapeToText()
+scrapeToMarkdown()
+# scrapeToHtml()
